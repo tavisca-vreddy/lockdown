d6aa6fe33a42da35d9ad3f6a1ee406de07fb3a22
tavisca-vreddy <51921004+tavisca-vreddy@users.noreply.github.com>
2020-10-08 08:36:18 +0530

    config-server-created

---
+++ 4_spring-confgi-client-server/myConfigServer/src/test/java/com/lockdown/myConfigServer/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockdown.myConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
